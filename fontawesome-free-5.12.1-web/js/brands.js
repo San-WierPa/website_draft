bd11502ef26718cf308437bbec2439ca646406e4
paripsa <paripsa@uni-wuppertal.de>
2022-06-04 21:45:06 +0200

    add fonts as lfs

---
+++ fontawesome-free-5.12.1-web/js/brands.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c1cb649d69cc625246a09e58f39575713ff8a37055c90c28164b96651a5c457e
+size 440863
---
+++ fontawesome-free-5.12.1-web/js/solid.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:991bfc116c2eed0735253ee998d59d8b3bf53e91a14e171cc0881a779a3c8c78
+size 588812
